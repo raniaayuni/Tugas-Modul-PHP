497c809ba9e0c16260fc8c857db350f181d21814
Rania Ayuni <raniaaayunii@gmail.com>
2022-12-19 13:04:11 +0700

    Tugas PHP

---
+++ Tugas1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    <h2>Latihan 1</h2>
+    <?php
+$num = 4;
+if ($num % 2 == 0) {
+  echo "$num adalah bilangan genap";
+} else {
+  echo "$num adalah bilangan ganjil";
+}
+    ?>
+</body>
+</html>
